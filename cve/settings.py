903f996e0605f1a06b3c0c14d9011f83473ab5b4
Gustavo Cevallos <gstavocevallos@gmail.com>
2016-09-03 18:39:53 -0500

    a

--- cve/settings.py
+++ cve/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,5 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+, 'staticfiles')
 STATIC_URL = '/static/'
